75cb152c754d5210280ff1f08ccb30d0dd3f4751
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2024-06-29 15:05:44 +0100

    initial commit

---
+++ src/com/ajcode2/Main.java
@@ -0,0 +1,23 @@
+package com.ajcode2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+
+
+        System.out.println("My name is " + name);
+        System.out.println("I am "+ age +" years old");
+        System.out.println("My favorite food is " +food);
+
+
+    }
+}
