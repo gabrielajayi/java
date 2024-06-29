ea6f5656638bab978c7928d6e9c4af35ca4ebf00
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2024-06-29 14:35:34 +0100

    Initial commit

--- src/com/ajcode/Main.java
+++ src/com/ajcode/Main.java
@@ -1,35 +1,23 @@
 package com.ajcode;
 
-/*
-- Access modifier - Public, private, protected or default
-- Optional - Static
-- Return type - void
-- Name -
-- Optional parameters - ARRAY - String[] args)
-- Method body - What goes between the opening curley brackets
-Optional return value - for the code below, it is void because it does not return any value
- */
-
-/*
- Exercise: - calculate the % rental yield on a property in a year
- - Monthly rental income: £1,300
- - Purchased property price: £250,000
-  */
+import java.util.Scanner;
 
 public class Main{
-
     public static void main(String[] args){
-    double yield = calculatePropertyYield(
-            1_300, 250_000
-                );
-        System.out.println(yield);
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
 
-    private static double calculatePropertyYield(
-            double rent, double propertyPrice
-    ) {
-        double yearlyRent = rent * 12;
-        return (yearlyRent / propertyPrice) * 100;
-    }
 
+        System.out.println("My name is " + name);
+        System.out.println("I am "+ age +" years old");
+        System.out.println("My favorite food is " +food);
+
+
+    }
 }
