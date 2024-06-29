bdee1b3b7c0c9068c9ffc76db334529a4e90f4f9
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2024-06-29 17:22:06 +0100

    Initial commit

---
+++ src/com/ajcode3/Main.java
@@ -0,0 +1,9 @@
+package com.ajcode3;
+// To display answers in double without displacing the values after the decimal
+public class Main {
+    public static void main(String[] args){
+        double friends = 10;
+         friends = (double) friends / 3;
+        System.out.println(friends);
+    }
+}
