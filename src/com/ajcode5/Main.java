107a0015e7e968019b4f90cf52d1d1d4bf04adc7
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2024-07-02 00:47:56 +0100

    Initial commit

---
+++ src/com/ajcode5/Main.java
@@ -0,0 +1,25 @@
+package com.ajcode5;
+
+import java.util.Scanner;
+
+// Finding hypotenuse using the math class
+public class Main {
+    public static void main(String[] args){
+        double x;
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter side x:");
+        x = scanner.nextDouble();
+        System.out.println("Enter side x:");
+        y = scanner.nextDouble();
+
+       z = Math.sqrt((x*x)+(y*y));
+
+        System.out.println("The hypotenuse is: " + z);
+
+        scanner.close();
+    }
+}
